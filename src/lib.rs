7ce497b940e82b5c2d8b8013e57fc6a50b240861
K0R0VA <yarykov.m@edu.narfu.ru>
2021-09-11 21:51:54 +0300

    Исправил пути к трейтам

--- src/lib.rs
+++ src/lib.rs
@@ -5,32 +5,30 @@ extern crate proc_macro;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-
 #[proc_macro_derive(PrimaryKey)]
 pub fn primary_key_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
     TokenStream::from(quote! {
-        use tokio_postgres::types::ToSql;
-        impl ToSql for #name {
-            fn to_sql(&self, ty: &tokio_postgres::types::Type, out: &mut BytesMut) -> Result<tokio_postgres::types::IsNull, Box<dyn std::error::Error + Sync + Send>>
+        impl tokio_postgres::types::ToSql for #name {
+            fn to_sql(&self, ty: &tokio_postgres::types::Type, out: &mut actix_web::web::BytesMut) -> Result<tokio_postgres::types::IsNull, Box<dyn std::error::Error + Sync + Send>>
             where
                 Self: Sized {
-                <i32 as ToSql>::to_sql(&self.0, ty, out)
+                <i32 as tokio_postgres::types::ToSql>::to_sql(&self.0, ty, out)
             }
-        
+
             fn accepts(ty: &tokio_postgres::types::Type) -> bool
             where
                 Self: Sized {
-                <i32 as ToSql>::accepts( ty)
+                <i32 as tokio_postgres::types::ToSql>::accepts( ty)
             }
-        
+
             fn to_sql_checked(
                 &self,
                 ty: &tokio_postgres::types::Type,
                 out: &mut actix_web::web::BytesMut,
             ) -> Result<tokio_postgres::types::IsNull, Box<dyn std::error::Error + Sync + Send>> {
-                <i32 as ToSql>::to_sql_checked(&self, ty, out)
+                <i32 as tokio_postgres::types::ToSql>::to_sql_checked(&self, ty, out)
             }
         }
     })
